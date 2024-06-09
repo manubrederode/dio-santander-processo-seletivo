059a5553cecf2a28207874b988365dca520f7498
Manu Brederode <brederode.emmanuelle@gmail.com>
2024-06-09 16:27:10 +0100

    Add files via upload

---
+++ ControleFluxo/src/Contador.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Contador {
+
+    public static void main(String[] args) {
+
+        //recebendo os parâmetros
+        Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametroDois = terminal.nextInt();
+		
+		try {
+			//chamando o método contendo a lógica de contagem
+			contar(parametroUm, parametroDois);
+		
+		}catch (ParametrosInvalidosException exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro.");
+		}
+		
+	}
+
+    //método contar
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		//verificar se parametroUm é MAIOR que parametroDois e lançar a exceção.
+        if(parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+	
+		int contagem = parametroDois - parametroUm;
+		for (int i = 0; i < contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+	}
+}
